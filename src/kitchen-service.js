798ae14ab61df87a84d18810c48ab946c0e4673c
Student <example@example.com>
2020-02-18 12:29:23 -0800

    update kitchen-service.js to include back-end logic for calling spoonacualr API to recieve data on searching recipes by ingredients, update main.js to include a table to display recipe output information

--- src/kitchen-service.js
+++ src/kitchen-service.js
@@ -1,7 +1,8 @@
 export class KitchenService {
   async getRecipeByIngredient(ingredient) {
     try {
-      let response = await fetch(` https://api.spoonacular.com/recipes/findByIngredients?ingredients=${userInput}&number=2&apiKey=${process.env.API_KEY}`);
+      let response = await fetch(`https://api.spoonacular.com/recipes/findByIngredients?ingredients=${ingredient}&number=2&apiKey=${process.env.API_KEY}`);
+      
       let jsonifiedResponse;
       if (response.ok && response.status === 200) {
         jsonifiedResponse = await response.json();
