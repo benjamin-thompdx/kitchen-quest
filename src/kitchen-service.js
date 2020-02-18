046cc2cc309f234957f2e6d49f9575ef158df3f9
Jeremy Kale Padot <jeremy@padot.us>
2020-02-18 15:02:29 -0800

    initial

---
+++ src/kitchen-service.js
@@ -0,0 +1,17 @@
+export class KitchenService {
+  async getRecipeByIngredient(ingredient) {
+    try {
+      let response = await fetch(`https://api.spoonacular.com/recipes/findByIngredients?ingredients=${ingredient}&number=2&apiKey=${process.env.API_KEY}`);
+      
+      let jsonifiedResponse;
+      if (response.ok && response.status === 200) {
+        jsonifiedResponse = await response.json();
+      } else {
+        jsonifiedResponse = false;
+      }
+      return jsonifiedResponse;
+    } catch (error) {
+      return false;
+    }
+  }
+}
